a07e9ea5dbb7a02b68b47c9c67adc96e5f8b1a79
Andrew Weddle <aweddle@starrez.com>
2018-08-22 21:09:23 +1000

    Removed collate Please enter the commit message for your changes. Lines starting

--- Config/Schema/schema.php
+++ Config/Schema/schema.php
@@ -10,13 +10,13 @@ class AdHocReportingSchema extends CakeSchema {
     
 	public $ad_hoc_reports = array(
 		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'unsigned' => true, 'key' => 'primary'),
-		'title' => array('type' => 'string', 'null' => true, 'length' => 100, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
-		'options' => array('type' => 'text', 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'title' => array('type' => 'string', 'null' => true, 'length' => 100),
+		'options' => array('type' => 'text', 'null' => false, 'default' => null),
 		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
 		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
 		'indexes' => array(
 			'PRIMARY' => array('column' => 'id', 'unique' => 1)
 		),
-		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+		'tableParameters' => array('charset' => 'latin1')
     );
 }
